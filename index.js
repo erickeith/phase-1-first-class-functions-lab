18e02371f68a5224e40c0e87972315924cbb8857
Eric <eric.keith@flatironschool.com>
2023-01-02 20:16:36 -0600

    commit tree 9f62c9bf950557845c637c8b75d0c94d6e54ae32

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x) {
+    return fare => fare * x
+}
+
+function fareDoubler(fare) {
+    const doubler = fare * 2;
+    return doubler;
+}
+
+function fareTripler(fare) {
+    const tripler = fare * 3;
+    return tripler
+}
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers)
+}
